c620571ddefe556f7c50eb5e9301e9e7e5b5c184
fabricio <fabcovalesci@gmail.com>
2020-07-12 20:29:53 -0300

    .

--- core/settings.py
+++ core/settings.py
@@ -68,7 +68,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -162,4 +161,3 @@ CSRF_COOKIE_SECURE=True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
