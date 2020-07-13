8c1ff043ce3604bd66b5a8472bdbd6050dd935f4
fabricio <fabcovalesci@gmail.com>
2020-07-13 19:56:53 -0300

    criado paginas about e contato, e ja configurado urls, e views.

--- core/artigo/views.py
+++ core/artigo/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contato(request):
+    return render(request, 'contato.html')
