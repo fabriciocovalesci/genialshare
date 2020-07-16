298dc337ceac06b13c5f20f02d6d67cd88f10976
fabricio <fabcovalesci@gmail.com>
2020-07-16 13:03:02 -0300

    add ultimas paginas

--- core/artigo/urls.py
+++ core/artigo/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('artigos/', views.artigos, name='artigos'),
+    path('tutoriais/', views.tutoriais, name='tutoriais'),
     path('about/', views.about, name='about'),
     path('contato/', views.contato, name='contato'),
 ]
--- core/artigo/views.py
+++ core/artigo/views.py
@@ -3,10 +3,21 @@ from core.artigo.models import Artigo
 
 def home(request):
     post = Artigo.objects.all()
-    return render(request, 'base.html', {'post' : post})
+    return render(request, 'home.html', {'post' : post})
+
+
+def artigos(request):
+    return render(request, 'artigos.html')
+
+
+def tutoriais(request):
+    return render(request, 'tutoriais.html')
+
 
 def about(request):
     return render(request, 'about.html')
 
+
 def contato(request):
     return render(request, 'contato.html')
+
