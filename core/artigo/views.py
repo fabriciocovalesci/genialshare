6184f4e78474b148ee3cf7b340bf66f95e46182d
fabricio <fabcovalesci@gmail.com>
2020-07-19 23:12:10 -0300

    adicionado urls

--- core/artigo/views.py
+++ core/artigo/views.py
@@ -3,7 +3,7 @@ from core.artigo.models import Artigo
 
 def home(request):
     post = Artigo.objects.all()
-    return render(request, 'base.html', {'post' : post})
+    return render(request, 'home.html')
 
 
 def artigos(request):
