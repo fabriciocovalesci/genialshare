edaf6bf1af713bcb5cf22cb1d8b6d2036baf76ce
fabricio <fabcovalesci@gmail.com>
2020-07-19 23:21:20 -0300

    editado urls

--- core/artigo/urls.py
+++ core/artigo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from core.artigo import urls
 from . import views
 
 urlpatterns = [
+   
     path('', views.home, name='home'),
     path('artigos/', views.artigos, name='artigos'),
     path('tutoriais/', views.tutoriais, name='tutoriais'),
--- core/urls.py
+++ core/urls.py
@@ -19,14 +19,13 @@ from core.artigo import urls
 from core.tutorial import urls
 from django.conf.urls.static import static
 from django.conf import settings
-import ckeditor_uploader.urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.artigo.urls')),
     path('', include('core.tutorial.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
